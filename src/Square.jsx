d8daa7dcb05582f4e6099762fe08ae289028fc9f
Jesus Suarez <84726337+jimypage0211@users.noreply.github.com>
2024-06-06 15:59:52 -0400

    first commit

---
+++ src/Square.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Square = (squareText) => {    
+  return (
+    <div className='square'>
+        <p>{squareText}</p>
+    </div>
+  )
+}
+
+export default Square
